2b6e954ebdf6f4b522dba13109c859279ab433e7
Vladimír Čunát <v@cunat.cz>
2021-07-02 11:39:06 +0200

    CI: add nix for apkg and kresd

--- distro/pkg/nix/top-level.nix
+++ distro/pkg/nix/top-level.nix
@@ -1,6 +1,16 @@
+{ extraDepsFor ? null }:
 with import <nixpkgs> {};
 
-(callPackage ./. { }).overrideAttrs (attrs: {
-  src = ./apkg-v{{ version }}.tar.gz;
-})
+(callPackage ./. { }).overridePythonAttrs (attrs:
+  {
+    src = ./apkg-v{{ version }}.tar.gz;
+  }
+  # We need extra tweaks in apkg CI:
+  // lib.optionalAttrs (extraDepsFor != null) {
+    buildInputs = attrs.buildInputs or []
+      ++ pkgs.${extraDepsFor}.buildInputs or [];
+    nativeBuildInputs = attrs.nativeBuildInputs or [] ++ [ gitMinimal ]
+      ++ pkgs.${extraDepsFor}.nativeBuildInputs or [];
+    }
+)
 
